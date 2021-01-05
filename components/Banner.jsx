de53257203b00a52a17e222b96953886578bf2e4
Velo <hartley.max@outlook.com>
2021-01-05 18:46:53 +0000

    content added to groups page. some styling work done. global colour added

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,8 +1,14 @@
 import style from '../styles/banner.module.css';
 
-export default function Banner({ title, image }) {
+export default function Banner({ title, image, position }) {
 	return (
-		<section className={style.banner} style={{ backgroundImage: `url(${image})` }}>
+		<section
+			className={style.banner}
+			style={{
+				backgroundImage: `url(${image})`,
+				backgroundPosition: `50% ${position || '50'}%`,
+			}}
+		>
 			<h1>{title}</h1>
 		</section>
 	);
--- pages/groups.jsx
+++ pages/groups.jsx
@@ -1,10 +1,81 @@
 import Nav from '../components/Nav';
+import Banner from '../components/Banner';
+import style from '../styles/groups.module.css';
 
 export default function Groups() {
 	return (
 		<>
 			<Nav />
-			<h1>Groups</h1>
+			<main>
+				<Banner title='Groups' image='/images/cafeFront.jpg' position='70' />
+				<section className={style.groups}>
+					<div className={style.group}>
+						<div className={style.day}>
+							<h2>Monday</h2>
+						</div>
+						<p>
+							Monmouth U3A
+							<br />
+							10:00am - 11:30am
+							<br /> Intermediate
+						</p>
+					</div>
+					<div className={style.group}>
+						<div className={style.day}>
+							<h2>Tuesday</h2>
+						</div>
+						<p>
+							Ross-On-Wye
+							<br /> 7:00pm - 8:30pm
+							<br /> Advanced
+						</p>
+					</div>
+					<div className={style.group}>
+						<div className={style.day}>
+							<h2>Wednesday</h2>
+						</div>
+						<p>
+							Abergavenny
+							<br /> 10:00am - 11:30am
+							<br /> Intermediate
+						</p>
+						<p>
+							Llangarron
+							<br /> 1:00pm - 2:30pm
+							<br /> Intermediate
+						</p>
+					</div>
+					<div className={style.group}>
+						<div className={style.day}>
+							<h2>Thursday</h2>
+						</div>
+						<p>
+							Monmouth
+							<br /> 10:00am - 11:30am
+							<br /> Conversation
+						</p>
+						<p>
+							Abergavenny
+							<br /> 4:00pm - 5:30pm
+							<br /> Advanced
+						</p>
+						<p>
+							Ross-On-Wye
+							<br /> 6:30pm - 8:00pm
+							<br /> Intermediate
+						</p>
+					</div>
+				</section>
+			</main>
 		</>
 	);
 }
+
+// <h2>Thursday</h2>
+// <p className={style.description}>Advanced Conversation Only</p>
+// <p>10:00am - 11:30am</p>
+// <p>Punch House Monmouth</p>
+// <p className={style.description}>Advanced Conversation and Translation</p>
+// <p>4:00pm - 5:30pm</p>
+// <p>Abergavenny</p>
+// </div>
